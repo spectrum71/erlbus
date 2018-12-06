71974bde55a02d52daf782efa8fdc0e9c2f2d780
Samuel Tupa Febrian <samuel.febrian@gmail.com>
2018-12-06 19:53:55 +0700

    Add fixed chatname feature

--- examples/chat/src/chat_cowboy_ws_handler.erl
+++ examples/chat/src/chat_cowboy_ws_handler.erl
@@ -8,8 +8,9 @@
 
 -define(CHATROOM_NAME, ?MODULE).
 -define(TIMEOUT, 5 * 60 * 1000). % Innactivity Timeout
+-define(DEFAULT_USERNAME, "-").
 
--record(state, {name, handler}).
+-record(state, {host, handler, username}).
 
 %% API
 
@@ -20,16 +21,21 @@ websocket_init(_Type, Req, _Opts) ->
   % Create the handler from our custom callback
   Handler = ebus_proc:spawn_handler(fun chat_erlbus_handler:handle_msg/2, [self()]),
   ebus:sub(Handler, ?CHATROOM_NAME),
-  {ok, Req, #state{name = get_name(Req), handler = Handler}, ?TIMEOUT}.
+  {ok, Req, #state{host = get_host(Req), handler = Handler, username = ?DEFAULT_USERNAME}, ?TIMEOUT}.
 
 websocket_handle({text, Msg}, Req, State) ->
-  ebus:pub(?CHATROOM_NAME, {State#state.name, Msg}),
-  {ok, Req, State};
+  if
+    State#state.username == ?DEFAULT_USERNAME ->
+      {ok, Req, #state{host = State#state.host, handler = State#state.handler, username = Msg}};
+    true ->
+      ebus:pub(?CHATROOM_NAME, {State#state.username, State#state.host, Msg}),
+      {ok, Req, State}
+  end;
 websocket_handle(_Data, Req, State) ->
   {ok, Req, State}.
 
-websocket_info({message_published, {Sender, Msg}}, Req, State) ->
-  {reply, {text, jiffy:encode({[{sender, Sender}, {msg, Msg}]})}, Req, State};
+websocket_info({message_published, {Sender, Host, Msg}}, Req, State) ->
+  {reply, {text, jiffy:encode({[{sender, Sender}, {host, Host}, {msg, Msg}]})}, Req, State};
 websocket_info(_Info, Req, State) ->
   {ok, Req, State}.
 
@@ -40,9 +46,9 @@ websocket_terminate(_Reason, _Req, State) ->
 
 %% Private methods
 
-get_name(Req) ->
+get_host(Req) ->
   {{Host, Port}, _} = cowboy_req:peer(Req),
-  Name = list_to_binary(string:join([inet_parse:ntoa(Host), 
+  Name = list_to_binary(string:join([inet_parse:ntoa(Host),
     ":", io_lib:format("~p", [Port])], "")),
   Name.
   
